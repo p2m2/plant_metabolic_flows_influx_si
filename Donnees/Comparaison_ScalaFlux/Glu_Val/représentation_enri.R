5871317f0f697d7a60912e2b21ae73aa7786235f
Anne-Gaelle <annegaelle@LAPTOP-FBV6R2K4.>
2024-06-11 09:28:20 +0200

    figure

---
+++ Donnees/Comparaison_ScalaFlux/Glu_Val/représentation_enri.R
@@ -0,0 +1,23 @@
+# Réprésentation de l'enrichissement glutamate vers valine
+library(ggplot2)
+
+donnees <- read.table("enrichissement_glu_val.tsv" , header = T, sep = "\t" , dec = ",")
+donnees$feuilles <- as.factor(donnees$feuilles)
+str(donnees)
+donnees
+
+figure <- ggplot (donnees , aes (x = feuilles , y = différence.enri , fill = replicat))+
+  geom_bar(stat = "identity" ,  position=position_dodge() ) +
+  labs (x = "Feuilles",
+        y = "Différence d'enrichissement (glu-val)" ,
+        title = "Pourcentage d'enrichissement en fonction des feuilles avec les différents réplicats",
+        subtitle = "Flux Glutamate vers Valine") +
+  theme(
+  plot.title = element_text(size = 20),      
+  plot.subtitle = element_text(size = 18),
+  axis.title = element_text(size = 20),    
+  axis.text = element_text(size = 20),
+  legend.title = element_text(size = 20),           
+  legend.text = element_text(size = 20) 
+)
+figure
